4fc48f71f5c9a5872395f961f09554b22024cf3f
Rishav-12 <rishavmitra10@gmail.com>
2022-11-02 16:24:00 +0530

    :sparkles: remove comments

--- markov_chain.py
+++ markov_chain.py
@@ -1,5 +1,3 @@
-# TODO: Write down a detailed step-by-step on how this works
-
 import random
 
 def load_shakespeare():
@@ -20,8 +18,6 @@ def load_subjects():
 
     return " ".join(txt_list).replace("\n", "").replace("\x00", "")
 
-# Text our chain will be based off of
-#txt = "Computer programming is the process of performing a particular computation (or more generally, accomplishing a specific computing result), usually by designing and building an executable computer program. Programming involves tasks such as analysis, generating algorithms, profiling algorithms' accuracy and resource consumption, and the implementation of algorithms (usually in a chosen programming language, commonly referred to as coding).[1][2] The source code of a program is written in one or more languages that are intelligible to programmers, rather than machine code, which is directly executed by the central processing unit. The purpose of programming is to find a sequence of instructions that will automate the performance of a task (which can be as complex as an operating system) on a computer, often for solving a given problem. Proficient programming thus usually requires expertise in several different subjects, including knowledge of the application domain, specialized algorithms, and formal logic."
 
 txt = load_shakespeare()
 order = 4
